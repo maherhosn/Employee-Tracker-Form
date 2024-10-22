39e031d7e4296f3557a886a781af2ec11e657bdd
Maher Aboulhosn <maherhosn@gmail.com>
2024-10-22 09:11:37 -0400

    specified the src/server and and connection details... \n ... dependencies needed (express, pg, typescript, inquired, and node)

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import express from 'express';
+import { QueryResult } from 'pg';
+import { pool, connectToDb } from './connection.js';
+
+await connectToDb();
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// for every selection made in the index folder create a query that would fetch the information from 
+// the corresponding classes.
+
+app.use((_req, res) => {
+  res.status(404).end();
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
