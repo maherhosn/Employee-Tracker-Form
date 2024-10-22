c03a0e1fef2497283097eba897c7f3edf5f8edda
Maher Aboulhosn <maherhosn@gmail.com>
2024-10-22 09:02:54 -0400

    pseudo code completed

---
+++ Classes/Employees.ts
@@ -0,0 +1,29 @@
+// WHEN I choose to view all employees
+// THEN I am presented with a formatted table showing employee data, including:
+// -- employee ids, first names, last names, job titles, 
+// -- departments, salaries, and managers that the employees report to
+
+
+// write a function that would query the database and return the table with
+// with employee ids, first names, last names, job titles, 
+// departments, salaries, and managers that the employees report to
+
+// =====================================================
+
+// WHEN I choose to add an employee
+// THEN I am prompted to enter:
+// --  the employee’s first name, last name, role, and manager, 
+// -- and that employee is added to the database
+
+// write a function that would connect to the database and insert into the table with a new entry:
+// --  the employee’s first name, last name, role, and manager, 
+// -- and that employee is added to the database
+
+// =================================================
+// WHEN I choose to update an employee role
+// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
+
+// write a function that would connect to the database and:
+// 1 (perform a select query to return all the employees and display it as a choise for the user to select)
+// 2 (allow the user to update the employees details,)
+// 3 (Create a query to update the database table with that information)
---
+++ Index.ts
@@ -0,0 +1,24 @@
+// AS A business owner
+// I WANT to be able to view and manage 
+// the departments, roles, and employees in my company
+// SO THAT I can organize and plan my business
+
+//import the poper packages and link the Classes together
+
+//Create an array of questions for the user to input
+
+// GIVEN a command-line application that accepts user input
+
+// WHEN I start the application
+// THEN I am presented with the following options: 
+// -- view all departments, view all roles, view all employees, 
+// -- add a department, add a role, add an employee, and update an employee role
+
+// Write a functin to connect to the Departments classes when the user selects anything anything
+// related to the department will be handled by the department.ts in the classes folder
+
+// Write a functin to connect to the employees classes when the user selects anything anything
+// related to the employees will be handled by the employees.ts in the classes folder
+
+// Write a functin to connect to the roles classes when the user selects anything anything
+// related to the roles will be handled by the roles.ts in the classes folder
